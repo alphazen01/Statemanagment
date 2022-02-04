e6d5ed7b26dad672227ceead7d575d2c26d927a9
alphazen01 <parvejmahmud555@gmail.com>
2022-02-04 17:41:39 +0600

    DateTime

--- lib/home.dart
+++ lib/home.dart
@@ -13,9 +13,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime ?_dateTime;
+  TimeOfDay?_timeOfDay;
  final themeController=Get.put(ThemeController());
 
 final logicController=Get.put(LogicController());
+
+
+getDate()async{
+  DateTime? date = await showDatePicker(
+    context: context, 
+    initialDate: DateTime(DateTime.now().year ), 
+    firstDate:  DateTime(DateTime.now().year -30), 
+    lastDate: DateTime(DateTime.now().year +30), );
+    setState(() {
+      _dateTime=date;
+    });
+}
+getTime()async{
+  TimeOfDay? time=await showTimePicker(
+    context: context, 
+    initialTime: TimeOfDay.now());
+    setState(() {
+      _timeOfDay=time;
+    });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +68,7 @@ final logicController=Get.put(LogicController());
           ),
         ), 
           appBar: AppBar(
-            backgroundColor: Colors.red,
+            // backgroundColor: Colors.red,
             title: Text("Statemanagment"),
             actions: [
              ObxValue(
@@ -66,7 +88,28 @@ final logicController=Get.put(LogicController());
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-             
+              _dateTime==null?Text("chose date"):
+              Text("Date: ${_dateTime?.year}/${_dateTime?.month}/${_dateTime?.day}",
+              style: TextStyle(fontSize: 40),
+              ),
+              
+             ElevatedButton(
+               onPressed: (){
+                 getDate();
+               }, 
+               child: Text("Date")
+               ),
+               _timeOfDay==null?Text("chose time"):
+              Text("Time: ${_timeOfDay?.hour}  : ${_timeOfDay?.minute}",
+              style: TextStyle(fontSize: 40),
+              ),
+              
+                 ElevatedButton(
+               onPressed: (){
+                 getTime();
+               }, 
+               child: Text("Time")
+               ),
               Obx(
                 ()=> Text("Count Number:${logicController.count}",
                  style: TextStyle(
